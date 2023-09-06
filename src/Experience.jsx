b06d90386ce285408cf3fb60c51bf3700baceb6f
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-09-06 15:52:40 +0200

    [testing]

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,16 +1,36 @@
 import { OrbitControls, Text3D, Center, useMatcapTexture } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
+import { useEffect, useRef } from 'react'
+import * as THREE from 'three'
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
 
 export default function Experience()
 {
 
+    const donuts = useRef([])
+
     const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+    useEffect(() =>
+    {
+        matcapTexture.colorSpace = THREE.SRGBColorSpace
+        matcapTexture.needsUpdate = true
 
-    // const tempArray = [...Array(100)]
-    // tempArray.map(() =>
-    // {
+        material.matcap = matcapTexture
+        material.needsUpdate= true
 
-    // })
+        
+    }, [])
+
+    useFrame((state, delta) =>
+    {
+        for (const donut of donuts.current) {
+            donut.rotation.y += delta * 0.2
+        }
+    })
+    
 
     return <>
 
@@ -23,6 +43,7 @@ export default function Experience()
         
         <Center>
             <Text3D font={'./fonts/helvetiker_regular.typeface.json'}
+                material={material}
                 size={0.75}
                 height={0.2}
                 curveSegments={12}
@@ -33,29 +54,29 @@ export default function Experience()
                 bevelSegments={5}
             >
                 HELLO R3F
-                <meshMatcapMaterial matcap={matcapTexture} />
             </Text3D>
         </Center>
-
-        {[...Array(100)].map((value, index) =>
-            <mesh
-                key={index}
-                position={[
-                    (Math.random() - 0.5) * 10,
-                    (Math.random() - 0.5) * 10,
-                    (Math.random() - 0.5) * 10
-                ]}
-                scale={ 0.2 + Math.random() * 0.2}
-                rotation={[
-                    Math.random() * Math.PI,
-                    Math.random() * Math.PI,
-                    0
-                ]}
-            >
-                <torusGeometry args={[1, 0.6, 16, 32]} />
-                <meshMatcapMaterial matcap={matcapTexture} />
-            </mesh>                
-        )}
+        
+            {[...Array(100)].map((value, index) =>
+                <mesh
+                ref={(element) => donuts.current[index] = element}
+                    geometry={torusGeometry}
+                    material={material}
+                    key={index}
+                    position={[
+                        (Math.random() - 0.5) * 10,
+                        (Math.random() - 0.5) * 10,
+                        (Math.random() - 0.5) * 10
+                    ]}
+                    scale={ 0.2 + Math.random() * 0.2}
+                    rotation={[
+                        Math.random() * Math.PI,
+                        Math.random() * Math.PI,
+                        0
+                    ]}
+                />              
+                            
+            )}
         
     </>
 }
