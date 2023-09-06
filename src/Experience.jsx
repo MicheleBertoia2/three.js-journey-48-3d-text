b989bcd9ef630af17c2fd501aeb68bfdce36eadd
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-09-06 15:32:39 +0200

    [first commit]

---
+++ src/Experience.jsx
@@ -0,0 +1,61 @@
+import { OrbitControls, Text3D, Center, useMatcapTexture } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+
+export default function Experience()
+{
+
+    const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+
+    // const tempArray = [...Array(100)]
+    // tempArray.map(() =>
+    // {
+
+    // })
+
+    return <>
+
+        
+
+        <Perf position="top-left" />
+
+        <OrbitControls makeDefault />
+
+        
+        <Center>
+            <Text3D font={'./fonts/helvetiker_regular.typeface.json'}
+                size={0.75}
+                height={0.2}
+                curveSegments={12}
+                bevelEnabled
+                bevelThickness={0.02}
+                bevelSize={0.02}
+                bevelOffset={0}
+                bevelSegments={5}
+            >
+                HELLO R3F
+                <meshMatcapMaterial matcap={matcapTexture} />
+            </Text3D>
+        </Center>
+
+        {[...Array(100)].map((value, index) =>
+            <mesh
+                key={index}
+                position={[
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10
+                ]}
+                scale={ 0.2 + Math.random() * 0.2}
+                rotation={[
+                    Math.random() * Math.PI,
+                    Math.random() * Math.PI,
+                    0
+                ]}
+            >
+                <torusGeometry args={[1, 0.6, 16, 32]} />
+                <meshMatcapMaterial matcap={matcapTexture} />
+            </mesh>                
+        )}
+        
+    </>
+}
